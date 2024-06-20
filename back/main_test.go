41c552be9825d61449b739a0a1088672d4cff87a
German <german@centraal.studio>
2024-06-20 17:39:16 -0500

    minimal example for multiples handlers

--- back/main_test.go
+++ back/main_test.go
@@ -6,18 +6,41 @@ import (
 	"testing"
 )
 
-func TestHelloHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "/", nil)
+func TestCreateHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/create", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(helloHandler)
+	handler := http.HandlerFunc(createHandler)
 
 	handler.ServeHTTP(rr, req)
 
-	expected := "Hello, Heroku!"
+	expected := "Create endpoint hit!"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestGetHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/get", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	expected := "Get endpoint hit!"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
