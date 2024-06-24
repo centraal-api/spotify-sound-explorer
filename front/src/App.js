2ac95c0f777c9efa06f990250a3ca386c1b159a2
German <german@centraal.studio>
2024-06-24 14:11:58 -0500

    testing the button

--- front/src/App.js
+++ front/src/App.js
@@ -3,17 +3,18 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
+  const fetchMessage = () => {
     fetch('https://spotify-sound-explorer-cc4f60bc745e.herokuapp.com/get') // Replace with your backend API endpoint
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  };
 
   return (
     <div>
       <h1>Message from Backend:</h1>
       <p>{message}</p>
+      <button onClick={fetchMessage}>Fetch Message</button>
     </div>
   );
 }
